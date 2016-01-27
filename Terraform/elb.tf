4aab2ba2ef82eb829d5e3467461a3dca721fb128
Remco Overdijk <remco@maxserv.com>
2016-01-27 18:17:00 +0100

    Updated 1 license

--- Terraform/elb.tf
+++ Terraform/elb.tf
@@ -1,3 +1,9 @@
+/* Copyright 2016 Remco Overdijk - MaxServ B.V.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 resource "aws_elb" "demo-maxserv-com" {
   name = "demo-maxserv-com"
   # Although the webservers live in private subnets, the ELB's have to be in a PUBLIC subnet to be connected.
